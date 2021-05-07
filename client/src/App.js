91d5cd53bab980b570c3b7c5baa57539813f1e6f
mazarag2 <mazarag2@gmail.com>
2021-05-07 14:36:55 -0700

    add components/routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Route,Link } from 'react-router-dom'
+import Fib from './Fib';
+import About from './About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to = "/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+          <div>
+            <Route exact path="/" component={Fib}/>
+            <Route path="/about" component={About}/>
+          </div>
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,9 +1,82 @@
-import React,{ Component } from 'react';
+import React,{ Component,useState,useEffect } from 'react';
 import axios from 'axios';
 
 const Fib = () => {
 
-    
+    const [indexes,updateIndexes]  = useState([]);
+    const [values,updateValues] = useState({});
+    const [seenIndexes,UpdateSeenIndexes] = useState([]);
+    const API_ENDPOINT_CURRENT = '/api/values/current';
+    const API_ENDPOINT_ALL = 'api/api/values';
+
+    const fetchValues = async() => {
+        const values = await axios.get(API_ENDPOINT_CURRENT);
+        updateValues(values.data);
+
+    }
+
+    const fetchIndexes = async() => {
+
+        const seenIndexes = await axios.get(API_ENDPOINT_ALL);
+        UpdateSeenIndexes(seenIndexes.data)
 
 
-}
+    }
+
+    const renderValues = () => {
+        const entries = [];
+
+        for(let key in values){
+            entries.push(
+                <div key = {key}>
+                    For index {key} I calculated {values[key]}
+                </div>
+            )
+        }
+        return entries;
+    }
+
+    const handleSubmit = async(event) => {
+
+        event.preventDefault();
+        await axios.post('api/values',{
+            index : indexes
+        })
+        updateIndexes('')
+
+    }
+    useEffect(async () => {
+
+        await fetchValues();
+        await fetchIndexes();
+
+    },[])
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Enter your Index</label>
+                <input 
+                value={indexes}
+                onChange={event => updateIndexes({ indexes : event.target.value })}
+
+                />
+                <button>Submit</button>
+            </form>
+
+            <h3>Indexes I have seen:</h3>
+            {seenIndexes.map(({ number }) => number).join(', ')}
+
+            <h3>Calculated Values:</h3>
+            {
+                renderValues()
+            }
+        </div>
+    );
+
+
+
+
+
+};
+export default Fib
