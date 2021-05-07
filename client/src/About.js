6ed6cc0abcc32a52fe860b922804974e7e6da7af
mazarag2 <mazarag2@gmail.com>
2021-05-06 17:37:58 -0700

    add client and components

---
+++ client/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default () => {
+    return (
+        <div>
+            Sample Project for learning Multi continaer deployments with Docker and Kubernetes
+            <Link to="/">Go Back Home</Link>
+        </div>
+
+    )
+}
